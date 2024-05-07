fd776eb09fad7aac4fa7390ba9c35b17d268ef01
Biruk <israelbiruk10gmail.com>
2024-05-07 16:40:15 -0400

    new code

--- question1.py
+++ question1.py
@@ -3,28 +3,36 @@ import numpy as np
 #Part C#
 delta_t = 5
 g = 9.8
-mat_A = [[1, 0, delta_t, 0],
+mat_A = np.array([[1, 0, delta_t, 0],
          [0, 1, 0, delta_t],
          [0, 0, 1, 0],
          [0, 0, 0, 1],
-         ]
-mat_B = [[1, 0, 0, 0],
+         ])
+mat_B = np.array([[1, 0, 0, 0],
          [0, 1, 0, 0],
          [0, 0, 1, 0],
          [0, 0, 0, 1]
-         ]
-mat_C = [[1, 0, 0, 0],
+         ])
+mat_C = np.array([[1, 0, 0, 0],
          [0, 1, 0, 0]
-         ]
-u_t = [[0],
-       [0],
-       [0],
-       [-g*delta_t]
-       ]
+         ])
+u_t = np.array([0,0, 0, -g*delta_t]).T
+       
+x_initial = np.array([0, 0, 100, 100]).T 
 
-def calc_prior_mean(x_t, u_t, r_t):
+P_initial = np.array([[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 25, 5],
+         [0, 0, 5, 25]
+         ])
+
+
+def calc_prior_mean(x_t, u_t, t):
+    if t == 0:
+        pass
     pass
 
 def calc_prior_cov(P_new):
     pass
+
 print("asnwer")
