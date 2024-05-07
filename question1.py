c1432c1f52ee74fa0f3ef22a9d5a34a395bf9bdd
Osahon <osahonodiase1234@gmail.com>
2024-05-07 17:19:46 -0400

    cov

--- question1.py
+++ question1.py
@@ -31,9 +31,21 @@ def calc_prior_mean(A_t, x_t, u_t, t):
         new_u_t = np.array([0,0,0,-g*t])
 
         return mat_A @ calc_prior_mean(A_t, x_t, new_u_t, t-1) + mat_B @ new_u_t
-
-def calc_prior_cov(P_new):
-    pass
-
+Q = np.eye(4)
+def calc_prior_cov(P_new, mat_A, t, P_initial, Q):
+    if t == 0:
+        A_new = mat_A
+        curr = (A_new @ P_initial @ A_new.T) + Q
+        return curr, t
+    elif t > 0:
+        P_sub, new_t = calc_prior_cov(P_new, mat_A, t - 1, P_initial, Q)
+        A_sub = np.array([[1, 0, new_t, 0],
+                          [0, 1, 0, new_t],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
+        curr = (A_sub @ P_sub @ A_sub.T) + Q
+        return curr, new_t + 1
+    return None, None 
+print("Covariance matrix for 5|5", calc_prior_cov(P_initial, mat_B, 5, P_initial, Q))
 c_mean_answer = calc_prior_mean(mat_B, x_initial,u_initial, 5)
 print("Prior Mean: ", c_mean_answer)
