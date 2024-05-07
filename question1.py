5acf89ceb216676859367d28e5d6ee36dc7cb84a
Biruk <israelbiruk10gmail.com>
2024-05-07 17:00:27 -0400

    mean code

--- question1.py
+++ question1.py
@@ -3,17 +3,14 @@ import numpy as np
 #Part C#
 
 g = 9.8
-mat_B = np.array([[1, 0, 0, 0],
-         [0, 1, 0, 0],
-         [0, 0, 1, 0],
-         [0, 0, 0, 1]
-         ])
+mat_B = np.eye(4)
 mat_C = np.array([[1, 0, 0, 0],
          [0, 1, 0, 0]
          ])
 
        
 x_initial = np.array([0, 0, 100, 100]).T 
+u_initial = np.array([0,0,0,0]).T
 
 P_initial = np.array([[0, 0, 0, 0],
          [0, 0, 0, 0],
@@ -22,20 +19,20 @@ P_initial = np.array([[0, 0, 0, 0],
          ])
 
 
-def calc_prior_mean(x_t, u_t, t):
+def calc_prior_mean(A_t, x_t, u_t, t):
     if t == 0:
-        return (x_initial, np.array([0,0,0,0]).T)
+        return A_t @ x_t + mat_B @ u_t
 
     else:
         mat_A = np.array([[1, 0, t, 0],
-         [0, 1, 0, t],
-         [0, 0, 1, 0],
-         [0, 0, 0, 1],
-         ])
-        u_t = np.array([0,0, 0, -g*t]).T
-        return mat_A @ calc_prior_mean(x_t, u_t, t-1)[0] + mat_B @ calc_prior_mean(x_t, u_t, t-1) 
+                           [0, 1, 0, t],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+
+        return mat_A @ calc_prior_mean(A_t, x_t, u_t, t-1) + mat_B @ u_t
 
 def calc_prior_cov(P_new):
     pass
 
-print("asnwer")
+c_mean_answer = calc_prior_mean(mat_B, x_initial,u_initial, 5)
+print("Prior Mean: ", c_mean_answer)
