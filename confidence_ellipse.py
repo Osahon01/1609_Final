bf2fb2f7c2ccac7f711a59e821591808040c2d71
mattmosq <mattmosq@mit.edu>
2024-05-08 16:07:24 -0400

    plots

--- confidence_ellipse.py
+++ confidence_ellipse.py
@@ -3,13 +3,22 @@ from scipy.stats.distributions import chi2
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from question1 import mean, cov
+
+
+def nicegrid(ax):  # Shoutout nice grid function
+    ax.grid(True, which='major', color='#666666', linestyle=':')
+    ax.grid(True, which='minor', color='#999999', linestyle=':', alpha=0.2)
+    ax.minorticks_on()
 
 # inputs:
 # mu    = mean vector
 # Sigma = covariance matrix
 # alpha = level
 # color = plot color
-def confidence_ellipse( mu, Sigma, alpha, color ):
+
+
+def confidence_ellipse(mu, Sigma, alpha, color):
 
     # Sanity checks
     if not np.allclose(Sigma, Sigma.T, rtol=1e-05, atol=1e-08):
@@ -26,22 +35,36 @@ def confidence_ellipse( mu, Sigma, alpha, color ):
     # Set up
     z = chi2.ppf(alpha, df=2)
 
-
     # eigenvalues, eigenvectors
-    eigenvalues, eigenvectors = LA.eig( Sigma )
+    eigenvalues, eigenvectors = LA.eig(Sigma)
     lambda1 = eigenvalues[0]
     lambda2 = eigenvalues[1]
-    theta0  = math.atan2( eigenvectors[1,0], eigenvectors[0,0]  )
+    theta0 = math.atan2(eigenvectors[1, 0], eigenvectors[0, 0])
 
     # Plot
     N = 50
     theta = np.linspace(0, 2*np.pi, num=N)
 
     # use polar coordinates to generate points on the ellipse
-    x = mu[0] + np.sqrt( lambda1 * z  )*np.cos(theta)*np.cos(theta0) - np.sqrt(lambda2*z)*np.sin(theta)*np.sin(theta0)
-    y = mu[1] + np.sqrt( lambda1 * z  )*np.cos(theta)*np.sin(theta0) + np.sqrt(lambda2*z)*np.sin(theta)*np.cos(theta0)
+    x = mu[0] + np.sqrt(lambda1 * z)*np.cos(theta)*np.cos(theta0) - \
+        np.sqrt(lambda2*z)*np.sin(theta)*np.sin(theta0)
+    y = mu[1] + np.sqrt(lambda1 * z)*np.cos(theta)*np.sin(theta0) + \
+        np.sqrt(lambda2*z)*np.sin(theta)*np.cos(theta0)
 
-    plt.plot( x, y, color  )
-    # FEEL FREE TO CHANGE FORMATTING
-    plt.savefig('ellipse.png' )  
+    plt.plot(x, y, color)
 
+
+alphas = [0.90, 0.95]
+colors = ['red', 'blue']
+
+for interval, color in zip(alphas, colors):
+    confidence_ellipse(mean, cov, interval, color)
+
+plt.gca().set_aspect('equal', adjustable='box')
+plt.xlabel('Horizontals Label')
+plt.ylabel('Vertical Position')
+plt.title('Confidence Ellipses')
+nicegrid(plt.gca())
+plt.legend([f'{alpha*100}% Confidence Ellipse' for alpha in alphas])
+plt.savefig('confidence_ellipses.png')
+plt.show()
--- question1.py
+++ question1.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-#Part C#
+# Part C#
 
 g = 9.8
 mat_B = np.eye(4)
 mat_C = np.array([[1, 0, 0, 0],
-         [0, 1, 0, 0]
-         ])
+                  [0, 1, 0, 0]
+                  ])
 
-       
-x_initial = np.array([0, 0, 100, 100]).T 
-u_initial = np.array([0,0,0,0]).T
+
+x_initial = np.array([0, 0, 100, 100]).T
+u_initial = np.array([0, 0, 0, 0]).T
 
 P_initial = np.array([[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 25, 5],
-         [0, 0, 5, 25]
-         ])
+                      [0, 0, 0, 0],
+                      [0, 0, 25, 5],
+                      [0, 0, 5, 25]
+                      ])
 
 
 def calc_prior_mean(A_t, x_t, u_t, t):
@@ -25,26 +25,33 @@ def calc_prior_mean(A_t, x_t, u_t, t):
 
     else:
         mat_A = np.array([[1, 0, t, 0],
-                           [0, 1, 0, t],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]])
-        new_u_t = np.array([0,0,0,-g*t])
+                          [0, 1, 0, t],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
+        new_u_t = np.array([0, 0, 0, -g*t])
 
         return mat_A @ calc_prior_mean(A_t, x_t, new_u_t, t-1) + mat_B @ new_u_t
-    
+
+
 Q = np.eye(4)
+
+
 def calc_prior_cov(A_t, P_t, Q, t):
     if t == 0:
         return (A_t @ P_t @ A_t.T) + Q
 
     else:
         mat_A = np.array([[1, 0, t, 0],
-                           [0, 1, 0, t],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]])
+                          [0, 1, 0, t],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
 
         P_new = calc_prior_cov(mat_A, P_t, Q, t-1)
         return (mat_A @ P_new @ mat_A.T) + Q
-    
-print("Prior Mean: ", calc_prior_mean(mat_B, x_initial,u_initial, 5) )
-print("Covariance matrix for 5|0", calc_prior_cov(mat_B, P_initial, Q,5))
+
+
+mean = calc_prior_mean(mat_B, x_initial, u_initial, 5)
+cov = calc_prior_cov(mat_B, P_initial, Q, 5)
+
+print("Prior Mean: ", mean)
+print("Covariance matrix for 5|0", cov)
