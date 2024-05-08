e4c7f1f142b7cc4f3ef7586b44e96087cca512ed
Biruk <israelbiruk10gmail.com>
2024-05-08 14:56:53 -0400

    a

--- question2.py
+++ question2.py
@@ -1,1 +1,45 @@
 import numpy as np
+g = 9.8
+mat_B = np.eye(4)
+mat_C = np.array([[1, 0, 0, 0],
+         [0, 1, 0, 0]
+         ])
+
+       
+x_initial = np.array([0, 0, 100, 100]).T 
+u_initial = np.array([0,0,0,0]).T
+
+P_initial = np.array([[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 25, 5],
+         [0, 0, 5, 25]
+         ])
+
+
+def calc_prior_mean(A_t, x_t, u_t, t):
+    if t == 0:
+        return A_t @ x_t + mat_B @ u_t
+
+    else:
+        mat_A = np.array([[1, 0, t, 0],
+                           [0, 1, 0, t],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+        new_u_t = np.array([0,0,0,-g*t])
+
+        return mat_A @ calc_prior_mean(A_t, x_t, new_u_t, t-1) + mat_B @ new_u_t
+    
+Q = np.eye(4)
+def calc_prior_cov(A_t, P_t, Q, t):
+    if t == 0:
+        return (A_t @ P_t @ A_t.T) + Q
+
+    else:
+        mat_A = np.array([[1, 0, t, 0],
+                           [0, 1, 0, t],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+
+        P_new = calc_prior_cov(mat_A, P_t, Q, t-1)
+        return (mat_A @ P_new @ mat_A.T) + Q
+    
