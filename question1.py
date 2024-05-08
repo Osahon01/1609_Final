6476c38e42a7e7575f8e840f4016b5d0ee335a7d
Matthew Mosqueda <matthew03mosqueda@gmail.com>
2024-05-08 17:17:50 -0400

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-# Part C#
+from confidence_ellipse import confidence_ellipse, nicegrid
+import matplotlib.pyplot as plt
+# Part C #
 
 g = 9.8
 mat_B = np.eye(4)
@@ -50,8 +51,49 @@ def calc_prior_cov(A_t, P_t, Q, t):
         return (mat_A @ P_new @ mat_A.T) + Q
 
 
-mean = calc_prior_mean(mat_B, x_initial, u_initial, 5)
-cov = calc_prior_cov(mat_B, P_initial, Q, 5)
+mean_5 = calc_prior_mean(mat_B, x_initial, u_initial, 5)
+cov_5 = calc_prior_cov(mat_B, P_initial, Q, 5)
 
-print("Prior Mean: ", mean)
-print("Covariance matrix for 5|0", cov)
+
+def plot_confidence_ellipses_d():
+    alphas = [0.9, 0.95]
+    colors = ['red', 'blue']
+    for interval, color in zip(alphas, colors):
+        confidence_ellipse(mean_5, cov_5, interval, color)
+
+
+print("Prior Mean: ", mean_5)
+print("Covariance matrix for 5|0", cov_5)
+
+
+def plot_confidence_ellipses_e():
+    alphas = [0.9]
+    colors = ['green', 'blue']
+    for time, color in zip([10, 15], colors):
+        mean_time = calc_prior_mean(mat_B, x_initial, u_initial, time)
+        cov_time = calc_prior_cov(mat_B, P_initial, Q, time)
+        confidence_ellipse(mean_time, cov_time, alphas[0], color)
+
+
+plt.figure(figsize=(8, 6))
+plt.xlabel('Horizontal Position (m)')
+plt.ylabel('Vertical Position (m)')
+plt.title('Confidence Ellipse(s) for $\\alpha$ and $t$')
+plt.gca().set_aspect('equal', adjustable='box')
+nicegrid(plt.gca())
+
+plot_confidence_ellipses_d()
+plt.legend(['90% Confidence Ellipse (t=5)', '95% Confidence Ellipse (t=5)'])
+plt.show()
+
+plt.savefig('answer_1d.svg', format='svg')
+
+
+plt.xlabel('Horizontal Position (m)')
+plt.ylabel('Vertical Position (m)')
+plt.title('Confidence Ellipse(s) for $\\alpha$ and $t$')
+nicegrid(plt.gca())
+plot_confidence_ellipses_e()
+plt.legend(['90% Confidence Ellipse (t=10)', '90% Confidence Ellipse (t=15)'])
+plt.show()
+plt.savefig('answer_1e.svg', format='svg')  # Save the plot for part e
