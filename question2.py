f561cc84567a2221028f8820147c0ff134362802
mattmosq <mattmosq@mit.edu>
2024-05-10 12:54:39 -0400

    added nice grid to question2 plots

--- question2.py
+++ question2.py
@@ -3,23 +3,24 @@ import scipy
 import scipy.stats as sci
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
+from confidence_ellipse import nicegrid
 
 
 g = 9.8
 mat_B = np.eye(4)
 mat_C = np.array([[1, 0, 0, 0],
-         [0, 1, 0, 0]
-         ])
+                  [0, 1, 0, 0]
+                  ])
 
-       
-x_initial = np.array([0, 0, 100, 100]).T 
-u_initial = np.array([0,0,0,0]).T
+
+x_initial = np.array([0, 0, 100, 100]).T
+u_initial = np.array([0, 0, 0, 0]).T
 
 P_initial = np.array([[0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 25, 5],
-         [0, 0, 5, 25]
-         ])
+                      [0, 0, 0, 0],
+                      [0, 0, 25, 5],
+                      [0, 0, 5, 25]
+                      ])
 
 
 def calc_prior_mean(A_t, x_t, u_t, t):
@@ -28,33 +29,39 @@ def calc_prior_mean(A_t, x_t, u_t, t):
 
     else:
         mat_A = np.array([[1, 0, t, 0],
-                           [0, 1, 0, t],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]])
-        new_u_t = np.array([0,0,0,-g*t])
+                          [0, 1, 0, t],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
+        new_u_t = np.array([0, 0, 0, -g*t])
 
         return mat_A @ calc_prior_mean(A_t, x_t, new_u_t, t-1) + mat_B @ new_u_t
-    
+
+
 Q = np.eye(4)
+
+
 def calc_prior_cov(A_t, P_t, Q, t):
     if t == 0:
         return (A_t @ P_t @ A_t.T) + Q
 
     else:
         mat_A = np.array([[1, 0, t, 0],
-                           [0, 1, 0, t],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]])
+                          [0, 1, 0, t],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
 
         P_new = calc_prior_cov(mat_A, P_t, Q, t-1)
         return (mat_A @ P_new @ mat_A.T) + Q
+
 
 t_chosen = 20
 # plt.show()
 
 # Define the normal distribution for x2 at time t = 20
-mean_x2 = calc_prior_mean(mat_B, x_initial, u_initial, t_chosen)[1]  # Mean of x2 at time t = 20
-cov_x2 = calc_prior_cov(mat_B, P_initial, Q, t_chosen)[1][1]  # Covariance of x2 at time t = 20
+mean_x2 = calc_prior_mean(mat_B, x_initial, u_initial, t_chosen)[
+    1]  # Mean of x2 at time t = 20
+cov_x2 = calc_prior_cov(mat_B, P_initial, Q, t_chosen)[
+    1][1]  # Covariance of x2 at time t = 20
 norm_x2 = scipy.stats.norm(mean_x2, cov_x2)
 
 # Calculate the cumulative distribution function (CDF) at t = 20
@@ -77,8 +84,11 @@ print("Probability that the projectile has landed by time 20:", prob_landed)
 print(mean_x2)
 
 
-#Part B#
-t_vals = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+# Part B#
+t_vals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+          16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+
 def prob_landed_by_t(t):
     x2_mean = calc_prior_mean(mat_B, x_initial, u_initial, t)[1]
     x2_var = calc_prior_cov(mat_B, P_initial, Q, t)[1][1]
@@ -90,7 +100,8 @@ def prob_landed_by_t(t):
 cdf_vals = [prob_landed_by_t(t) for t in t_vals]
 
 # Compute the PMF of Tland using the differences in CDF values
-pmf_vals = [cdf_vals[0]] + [max(0, cdf_vals[i] - cdf_vals[i - 1]) for i in range(1, len(cdf_vals))]
+pmf_vals = [cdf_vals[0]] + [max(0, cdf_vals[i] - cdf_vals[i - 1])
+                            for i in range(1, len(cdf_vals))]
 
 # Print the PMF values
 print("CDF of Tland:", cdf_vals)
@@ -103,7 +114,6 @@ plt.xlabel('Time')
 plt.ylabel('Probability')
 plt.title('PMF of Landing Time')
 plt.xticks(t_vals)
-plt.grid(axis='y', alpha=0.75)
+# plt.grid(axis='y', alpha=0.75)
+nicegrid(plt.gca())
 plt.show()
-
-
